f6d68ae0d03136bf9db8021a72014f8cf6dbc453
Artorias2077 <134543125+Artorias2077@users.noreply.github.com>
2023-05-24 14:34:12 -0400

    Archivos datos.py sin validar

---
+++ clase 12/#datos.py
@@ -0,0 +1,10 @@
+#datos.pi
+print("datos personales")
+print("--------------")
+vnom=input("ingrese su nombre")
+vedad=input("ingrese edad")
+print("----------------")
+print(f"su nombre es: {vnom}")
+print(f"su edad es: {vedad}")
+
+print("programa finalizado...")
